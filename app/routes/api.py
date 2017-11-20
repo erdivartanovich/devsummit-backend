e748d5dc5509a5a2736fa638a08732096f6a8735
erdivartanovich <erdivartanovich@gmail.com>
2017-11-20 15:59:37 +0700

    quick fix post questioner answer api

--- app/routes/api.py
+++ app/routes/api.py
@@ -1253,6 +1253,7 @@ def get_referal_info(id, *args, **kwargs):
 @api.route('/eventbrite/hook', methods=['POST'])
 def event_brite_hook():
     return EventBriteController.hook(request)
+    sss
 
 
 @api.route('/questioners', methods=['GET', 'POST'])
