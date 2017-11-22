42990d1d57bd171a8ae3073e886c8ba4e05c5126
andy-shi88 <shi88.andy@gmail.com>
2017-11-22 20:34:31 +0700

    dynamic email hackaton model

--- app/models/email_templates/email_hackaton.py
+++ app/models/email_templates/email_hackaton.py
@@ -5,14 +5,14 @@ from app.services.helper import Helper
 class EmailHackaton():
 	"""docstring for EmailPurchase"""
 
-	def __init__(self):
+	def __init__(self, file_name):
 		currpath = os.path.abspath(os.curdir)
-		file_path = "%s/%s" % ("app/models/email_templates", "devsummit-hackathon.html")
+		file_path = "%s/%s" % ("app/models/email_templates", file_name)
 		path = os.path.join(currpath, file_path)
 		f = codecs.open(path, 'r', 'utf-8')
 		template_list = f.read()
 		f.close()
 		self.template = str(template_list)
 
-	def build(self, name):
-		return self.template.replace('[ name ]', name)
+	def build(self, name, content=''):
+		return self.template.replace('[ name ]', name) + content
