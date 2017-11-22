aeb8710f799f273e794414cead1f8f67e0bbdd18
andy-shi88 <shi88.andy@gmail.com>
2017-11-22 20:36:31 +0700

    add send and resend certificate endpoint

--- app/services/hackaton_proposal_service.py
+++ app/services/hackaton_proposal_service.py
@@ -2,6 +2,9 @@ import datetime
 from flask import current_app, request
 from app.models import db, mail
 from app.models.email_templates.email_hackaton import EmailHackaton
+from app.models.user import User
+from app.models.user_ticket import UserTicket
+from app.models.check_in import CheckIn
 from sqlalchemy.exc import SQLAlchemyError
 from app.configs.constants import ROLE
 from app.configs.settings import LOCAL_TIME_ZONE
@@ -54,7 +57,7 @@ class HackatonProposalService(BaseService):
 		db.session.add(hackaton_proposal)
 		try:
 			db.session.commit()
-			mail_template = EmailHackaton()
+			mail_template = EmailHackaton("devsummit-hackathon.html")
 			user = hackaton_proposal.order.user
 			template = mail_template.build(user.first_name + ' ' + user.last_name)
 			email = emailservice.set_recipient(hackaton_proposal.order.user.email).set_subject('Indonesia Developer Summit 2017 Hackathon').set_sender('noreply@devsummit.io').set_html(template).build()
@@ -62,7 +65,7 @@ class HackatonProposalService(BaseService):
 			return response.set_data(hackaton_proposal.as_dict()).set_message('proposal succesfully created').build()
 		except SQLAlchemyError as e:
 			data = e.orig.args
-			return response.set_data(data).set_error(True).build()		
+			return response.set_data(data).set_error(True).build()
 
 	def check_hackaton_proposal_exist(self, user_id):
 		hackaton_proposal = db.session.query(HackatonProposal).join(Order).filter(Order.user_id == user_id).first()
@@ -112,12 +115,43 @@ class HackatonProposalService(BaseService):
 		hackaton_proposal = db.session.query(HackatonProposal).filter(HackatonProposal.order_id == payloads['order_id']).first()
 		if hackaton_proposal is None:
 			return response.set_error(True).set_data(None).set_message('proposal not found').build()
-		mail_template = EmailHackaton()
+		mail_template = EmailHackaton("devsummit-hackathon.html")
 		user = hackaton_proposal.order.user
 		template = mail_template.build(user.first_name + ' ' + user.last_name)
 		email = emailservice.set_recipient(user.email).set_subject('Indonesia Developer Summit 2017 Hackathon').set_sender('noreply@devsummit.io').set_html(template).build()
 		mail.send(email)
 		return response.set_data(None).set_message('email has been sent to: ' + user.email).build()
+
+	def resend_certificate(self, payloads):
+		response = ResponseBuilder()
+		emailservice = EmailService()
+		mail_template = EmailHackaton("devsummit-hackathon-certificate.html")
+		user = db.session.query(User).filter(User.id == payloads['user_id']).first()
+		if user is None:
+			return response.set_data(None).set_message('user not found').set_error(True).build()
+		extra = "\nLink to your downloadable certificate: <a href='https://api.devsummit.io/certificate-%s.pdf'>here</a>" %user.id
+		template = mail_template.build(user.first_name + ' ' + user.last_name, extra)
+		email = emailservice.set_recipient(user.email).set_subject('Indonesia Developer Summit 2017 Hackathon').set_sender('noreply@devsummit.io').set_html(template).build()
+		mail.send(email)
+		return response.set_data(None).set_message('email has been sent to: ' + user.email).build()
+
+	def send_certificate(self):
+		response = ResponseBuilder()
+		emailservice = EmailService()
+		mail_template = EmailHackaton("devsummit-hackathon-certificate.html")
+		checked_in = db.session.query(UserTicket.user_id).join(CheckIn).all()
+		hackers = db.session.query(User).filter(
+			User.role_id==ROLE['hackaton'], User.id.in_(checked_in)).all()
+		if hackers is None:
+			return response.set_data(None).set_message('user not found').set_error(True).build()
+		for user in hackers:
+			emailservice = EmailService()
+			extra = "\nLink to your downloadable certificate: <a href='https://api.devsummit.io/certificate-%s.pdf'>here</a>" %user.id
+			template = mail_template.build(user.first_name + ' ' + user.last_name, extra)
+			email = emailservice.set_recipient(user.email).set_subject('Indonesia Developer Summit 2017 Hackathon').set_sender('noreply@devsummit.io').set_html(template).build()
+			mail.send(email)
+
+		return response.set_data(None).set_message('email has been sent').build()
 
 	def transformTimeZone(self, obj):
 		entry = obj
